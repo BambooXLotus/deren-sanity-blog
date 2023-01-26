48b839a1fca93199907f3812a558ff10a07cc91f
Deren Somsanith <derensomsanith@gmail.com>
2023-01-26 17:11:51 -0600

    commit

--- src/app/(user)/head.tsx
+++ src/app/(user)/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Deren Blog</title>
+      <title>Deren Sanity Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
