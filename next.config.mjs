cc4b7670d381409167522a9edc5bb9500635be40
Deren Somsanith <derensomsanith@gmail.com>
2023-01-27 13:27:03 -0600

    try this.

--- next.config.mjs
+++ next.config.mjs
@@ -11,10 +11,10 @@ const config = {
   swcMinify: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+  // i18n: {
+  //   locales: ['en'],
+  //   defaultLocale: 'en',
+  // },
   experimental: {
     appDir: true,
   },
