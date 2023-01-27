c901f61f9ef96dc047721ccc33c2aaee4fbe8637
Deren Somsanith <derensomsanith@gmail.com>
2023-01-27 12:45:13 -0600

    change config to .js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-// @ts-check
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
-!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -21,4 +14,5 @@ const config = {
     domains: ['cdn.sanity.io'],
   },
 }
-export default config
+
+module.exports = config
